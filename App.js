6d3351787d96a5741cebee79dfdf2f80f8449c6b
Goncy <goncy@live.com.ar>
2016-04-03 12:51:56 +0100

    Video 4

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // With State
 class App extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <h2>I salute you</h2>
-      </div>
-    )
+    let txt = this.props.txt;
+    return <h1>{txt}</h1>
   }
 }
 
-// Tener default exporta solo una clase
-export default App;
+// Por motivo x, se tiene que declarar fuera de la clase
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+  txt: 'Default Goncy'
+}
+
+ReactDOM.render(
+  <App cat={5} txt="This is the props text" />,
+  document.getElementById('app')
+);
--- main.js
+++ main.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-
-// Con State
-ReactDOM.render(<App />, document.getElementById('app'));
