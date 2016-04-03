8bf29264c09b80c1dd8032cf8079edb48923cc9e
Goncy <goncy@live.com.ar>
2016-04-03 13:14:13 +0100

    Video 5

--- App.js
+++ App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 // With State
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'this is the state txt',
+      cat: 0
+    }
+  }
+
+  update (e) {
+    this.setState({
+      txt: e.target.value
+    });
+  }
+
   render() {
-    let txt = this.props.txt;
-    return <h1>{txt}</h1>
+    return (
+      <div>
+        <input type="text"
+          onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    )
   }
 }
 
-// Por motivo x, se tiene que declarar fuera de la clase
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-};
-
-App.defaultProps = {
-  txt: 'Default Goncy'
-}
-
-ReactDOM.render(
-  <App cat={5} txt="This is the props text" />,
-  document.getElementById('app')
-);
+export default App
